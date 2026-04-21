4c1bd256416c45abaf2935eea14c6b01dfbab0ed
mizahandrian <mizahandrian@gmail.com>
2026-04-21 10:35:56 +0300

    mande alo

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+Route::post('/login', [AuthController::class, 'login']);
